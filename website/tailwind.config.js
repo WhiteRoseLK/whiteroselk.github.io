7b426732258fe270fd352d56a846eca8a5175008
rankangi <mathieu.bannwarth@proton.me>
2024-05-21 22:52:07 +0200

    Update tailwind.config.js

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "../docs/**/*.mdx"],
   darkMode: ["class", '[data-theme="dark"]'], // hooks into docusaurus' dark mode settings
   theme: {
+    container: {
+      center: true
+    },
     colors: {
       white: "#fff",
       primary: {
